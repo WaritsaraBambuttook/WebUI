0a9c803ce399ded315d4f23ce75887d1e328ae1e
WaritsaraBambuttook <four_nukiz@hotmail.com>
2018-05-10 22:57:12 +0700

    ทำabout

--- js/search.js
+++ js/search.js
@@ -7,8 +7,8 @@ $(function () {
         PlaceSearch.Search(keyword, type, radius).then(function(data){
             for(var i=0;i<data.length;i++){
                 var row = 
-                '<a class="searchJS" href="detail.html?placeid=' + data[i].id + '">'+'<br><br>' + 
-                '<div class="row">' + 
+                '<a class="searchJS" href="detail.html?placeid=' + data[i].id + '">'+'<br>'+
+                '<div class="row borderresult">' + 
                     '<div class="col"><img src=' + data[i].photo + ' class="thumbnail" /></div>' +
                 '<div class="col">' + data[i].name + '</div>' +
                 '<div class="col">' + data[i].rating + '</div>' +
